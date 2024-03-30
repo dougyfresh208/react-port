46b05e9d15b7e47f51082ca2008be1eb1ced0389
Dougy Fresh <dougrouseltw@gmail.com>
2024-03-29 21:37:56 -0600

    moved src assets folder to public

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './AboutMe.css';
 import aboutMeData from '../../Data/aboutme.json'; // Adjust the path as necessary
-import profilePic from '../../assets/profile-pic.jpg';
+import profilePic from '/assets/profile-pic.jpg';
 function AboutMe() {
   return (
     <div className="about-container">
       <div className="left-side">
         <h1>{aboutMeData.intro}</h1>
-        <img src={profilePic} alt="Doug" className="doug-image" />
+        <img src={profilePic} alt="Profile" className="doug-image" />
       </div>
       <div className="right-side">
         {aboutMeData.paragraphs.map((paragraph, index) => (
