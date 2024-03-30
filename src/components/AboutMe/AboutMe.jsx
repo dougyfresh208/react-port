44269c680057557e281c41938555d2e7b079a596
Dougy Fresh <dougrouseltw@gmail.com>
2024-03-29 20:59:26 -0600

    updated image path in aboutMe.jsx

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './AboutMe.css';
 import aboutMeData from '../../Data/aboutme.json'; // Adjust the path as necessary
-
+import profilePic from '../../assets/profile-pic.jpg';
 function AboutMe() {
   return (
     <div className="about-container">
       <div className="left-side">
         <h1>{aboutMeData.intro}</h1>
-        <img src="./assets/profile-pic.jpg" alt="Doug" className="doug-image" />
+        <img src={profilePic} alt="Doug" className="doug-image" />
       </div>
       <div className="right-side">
         {aboutMeData.paragraphs.map((paragraph, index) => (
